3f5dd2a697318677e3a0822bea0942af7228ef87
LERUSH <112698670+azuREDO13@users.noreply.github.com>
2025-08-27 13:41:59 +0700

    Delete backend directory

--- backend/scripts/deleteWebhook.js
+++
@@ -1,8 +0,0 @@
-import('dotenv').then(async ({config})=>{
-  config();
-  const fetch = (await import('node-fetch')).default || global.fetch;
-  const { BOT_TOKEN } = process.env;
-  if(!BOT_TOKEN) throw new Error('BOT_TOKEN missing');
-  const r = await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/deleteWebhook`);
-  console.log(await r.json());
-});
